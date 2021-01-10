d6240609a42df019528d3f866e28ec0413f31c11
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-10 21:45:12 +0100

    Add function handleOnSubmit for button submit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 function App() {
+
+  function handleOnSubmit(e) {
+    e.preventDefault()
+    console.log("Form submitted")
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleOnSubmit}>
         <label>Email</label>
         <input />
         <label>Password</label>
         <input />
+        <button type="submit">Log in</button>
       </form>
     </div>
   );
