f37702dd0286a4fbb01628a44d0a9de535d8119c
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-10 21:52:55 +0100

    Add useRef for inputs email and password

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function App() {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   function handleOnSubmit(e) {
     e.preventDefault()
     console.log("Form submitted")
+    console.log(emailInputRef.current.value)
   }
 
   return (
     <div>
       <form onSubmit={handleOnSubmit}>
         <label>Email</label>
-        <input />
+        <input ref={emailInputRef} />
         <label>Password</label>
-        <input />
+        <input ref={passwordInputRef} />
         <button type="submit">Log in</button>
       </form>
     </div>
