c85be3abc548a6a1d24bf3a300c9e3ee06ba12f1
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-12 19:32:33 +0100

    Add CustomerListPage. Move to new page getCustomerList. Add useEffect for render list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import {Switch, Route} from 'react-router-dom';
 import LoginPage from './pages/LoginPage'
+import CustomerListPage from './pages/CustomerListPage'
+
 
 function App() {
-  const [customerList, setCustomerList] = useState([]);
   
   function getMe() {
     const url = "https://frebi.willandskill.eu/api/v1/me/";
@@ -18,35 +19,19 @@ function App() {
     .then(data => console.log(data))
   }
 
-  function getCustomerList() {
-    const url = "https://frebi.willandskill.eu/api/v1/customers/";
-    const token = localStorage.getItem("WEBB20");
-    fetch(url, {
-      headers:{
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-      }
-    })
-    .then(res => res.json())
-    .then(data => setCustomerList(data.results))
-  }
-
   return (
     <div>
       <Switch>
         <Route path="/login">
           <LoginPage />
         </Route>
+        <Route path="/customer-list">
+          <CustomerListPage />
+        </Route>
       </Switch>
       
       <hr />
-      <button onClick={getMe}>Get Me</button>
-      <button onClick={getCustomerList}>Get Customer List</button>
-
-      {customerList.map((item, index) => {
-        return <p key={index}>{item.name}</p>
-      })}
-
+      
     </div>
   );
 }
