a13d9b962de7c70e2a9d6e89a98c2d74fb641b7e
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-13 12:43:08 +0100

    Add useState and use Effect at CustomerDetailPage

--- src/pages/CustomerDetailPage.jsx
+++ src/pages/CustomerDetailPage.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function CustomerDetailPage() {
+export default function CustomerDetailPage(props) {
+  const customerId = props.match.params.id;
+  const [customerIten, setCustomerItem] = useState();
+
+  function getCustomerItem() {
+    const url = "https://frebi.willandskill.eu/api/v1/customers/${customerID}";
+    const token = localStorage.getItem("WEBB20");
+    fetch(url, {
+      headers:{
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => setCustomerItem(data.results))
+  }
+
+  useEffect( () => {
+    getCustomerItem()
+  }, [])
+
   return (
     <div>
       <h2>Customer Detail Page</h2>
