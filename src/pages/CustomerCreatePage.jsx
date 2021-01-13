ffc3fa2c70a51179c1301e8c21f47fe4e29cb906
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-13 15:29:04 +0100

    Add f renderInput at CustomerCreatePage

--- src/pages/CustomerCreatePage.jsx
+++ src/pages/CustomerCreatePage.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 export default function CustomerCreatePage() {
+
+  function renderInput(name, label) {
+    return (
+      <>
+      <label>{label}</label>
+      <input name={name} />
+      </>
+    )
+  }
+
   return (
     <div>
       <h2>Create Customer</h2>
       <form>
-        
+        {renderInput("name", "Customer Name")}
       </form>
     </div>
   )
