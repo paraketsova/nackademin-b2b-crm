8a9e443d2f308c4260aa7f2dc0bb221e601c1c26
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-13 15:36:44 +0100

    Update form at CustomerCreatePge witn input type

--- src/pages/CustomerCreatePage.jsx
+++ src/pages/CustomerCreatePage.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function CustomerCreatePage() {
 
-  function renderInput(name, label) {
+  function renderInput(name, label, type) {
     return (
-      <>
+      <div>
       <label>{label}</label>
-      <input name={name} />
-      </>
+      <input type={type || "text"} name={name} />
+      </div>
     )
   }
 
@@ -15,7 +15,14 @@ export default function CustomerCreatePage() {
     <div>
       <h2>Create Customer</h2>
       <form>
-        {renderInput("name", "Customer Name")}
+        {renderInput("name", "Customer Name")}{renderInput("email", "Customer Email", "email")}
+        {renderInput("organisationNr", "Organisation Number")}
+        {renderInput("vatNr", "Vat Number")}
+        {renderInput("reference", "Reference")}
+        {renderInput("paymentTerm", "Payment Term", "number")}
+        {renderInput("website", "Website", "url")}
+        {renderInput("email", "Customer Email", "email")}
+        {renderInput("phoneNumber", "Phone Number", "tel")}
       </form>
     </div>
   )
