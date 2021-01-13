83f3e8ce9a417a1f574f9ec2f72cdd81d793c7c6
Mariia Paraketsova <paraketsova@gmail.com>
2021-01-13 16:13:01 +0100

    add function handleOnChange for inputs at CustomerCreatePage

--- src/pages/CustomerCreatePage.jsx
+++ src/pages/CustomerCreatePage.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function CustomerCreatePage() {
+  const [formData, setFormData] = useState({})
+
+  function handleOnChange(e) {
+    const name = e.target.name
+    const value = e.target.value
+    const newObj = {...formData, [name]: value}
+    setFormData(newObj)
+  }
 
   function renderInput(name, label, type) {
     return (
       <div>
       <label>{label}</label>
-      <input type={type || "text"} name={name} />
+      <input 
+        type={type || "text"} 
+        name={name}
+        onChange={handleOnChange}
+      />
       </div>
     )
   }
